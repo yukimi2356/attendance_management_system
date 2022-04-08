11397125f1c12d14d9b34c44a1789395a6c716f8
yukimi.yamada <yukimi2356@gmail.com>
2022-04-08 22:41:00 +0900

    EncodingFilter作成

---
+++ src/main/java/filters/EncodingFilter.java
@@ -0,0 +1,29 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
