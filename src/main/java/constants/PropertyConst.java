d3266d5845ad2e13e3a7f2ef0740165b0a55b040
yukimi.yamada <yukimi2356@gmail.com>
2022-04-11 23:09:53 +0900

    定数ファイルとFrontController作成

---
+++ src/main/java/constants/PropertyConst.java
@@ -0,0 +1,16 @@
+package constants;
+
+public enum PropertyConst {
+
+    //ペッパー文字列
+    PEPPER("pepper");
+
+    private final String text;
+    private PropertyConst(final String text) {
+        this.text = text;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+}
